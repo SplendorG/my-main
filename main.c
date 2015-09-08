b7cc96e45fd03d9ac6d4c12b7ca83f97e17924f5
splendor <425356736@qq.com>
2015-09-08 03:55:24 -0700

    服务器1

--- main.c
+++ main.c
@@ -7,8 +7,18 @@ int main(void)
 	volatile struct sockaddr cli_addr = {0};
 	volatile char r_buff[] = "\0";
 	const char w_buff[] = "hello client";
-	const socklen_t = sizeof(struct sockaddr);	
+	const socklen_t cli_len = sizeof(struct sockaddr);	
 	
+	while(1)
+	{
+		recvfrom(ser_sock, r_buff, sizeof(r_buff), 0,
+				&cli_addr, &cli_len);
+
+		sendto(ser_sock, w_buff, strlen(w_buff), 0,
+				&cli_addr, cli_len);
+
+		printf("from client Msg:%s\n", r_buff);
+	}
 
 	return 0;
 }
